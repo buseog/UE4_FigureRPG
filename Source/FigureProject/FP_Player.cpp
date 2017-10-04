b5463f5be8e3be930211ad7e11dec59fd675d3fd
buseog <buseog1991@naver.com>
2017-10-05 03:10:50 +0900

    레벨 추가 스텟포인트 추가

--- Source/FigureProject/FP_Player.cpp
+++ Source/FigureProject/FP_Player.cpp
@@ -32,6 +32,11 @@ AFP_Player::AFP_Player()
 	SightSphere->SetSphereRadius(Status.AttackRange);
 
 	OnClicked.AddDynamic(this, &AFP_Player::ToggleStatus);
+
+	Level.Level = 1;
+	Level.Exp = 0.f;
+	Level.FullExp = 100.f;
+	Level.Point = 5;
 }
 
 
@@ -47,6 +52,10 @@ void AFP_Player::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	Regeneration(DeltaTime);
+	Level.CheckLevelUp();
+	Level.Exp += 1.f;
+	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, FString::Printf(TEXT("%f"), Status.Hp), true, FVector2D(5.f, 5.f));
 }
 
 void AFP_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -54,9 +63,31 @@ void AFP_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 }
 
+float AFP_Player::TakeDamage(float _Damage, struct FDamageEvent const& _DamageEvent, class AController* _EventInstigator, class AActor* _DamageCauser)
+{
+	HitTime = 1.f;
+
+	return Status.Hp;
+}
+
+void AFP_Player::Regeneration(float DeltaTime)
+{
+	HitTime -= DeltaTime;
+
+	if (HitTime >= 0.f)
+		return;
+
+	Status.Hp += Status.HpRegen;
+}
+
 
 void AFP_Player::StatusLevelUp(int _Type)
 {
+	if (Level.Point <= 0)
+		return;
+
+	Level.DecreasePoint();
+
 	switch (_Type)
 	{
 	case 0:
@@ -98,7 +129,7 @@ void AFP_Player::StatusLevelUp(int _Type)
 }
 
 
-void AFP_Player::ToggleStatus(AActor* p, FKey Button)
+void AFP_Player::ToggleStatus(AActor* AActor, FKey Button)
 {
 	APlayerController* Controller = UGameplayStatics::GetPlayerController(GetWorld(), 0);
 	Cast<AFP_PlayerController>(Controller)->ToggleStatus();
